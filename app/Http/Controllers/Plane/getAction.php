fdc18cbef199cc4b7397c9e8f229d9bfb81cb265
fcomarcet1 <fcomarcet1@gmail.com>
2021-10-18 16:59:38 +0200

    Add skeleton UserResource

--- app/Http/Controllers/Plane/getAction.php
+++ app/Http/Controllers/Plane/getAction.php
@@ -2,10 +2,17 @@
 
 namespace App\Http\Controllers\Plane;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Http\Resources\UserResource;
 
 class getAction extends Controller
 {
-    //
+
+    public function __invoke()
+    {
+        return UserResource::collection(User::all());
+    }
+    
 }
